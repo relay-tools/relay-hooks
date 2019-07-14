a7f81db0554ab2f5fb6c6ca65f2178aa027ebf6b
morrys <lorenzo.digiacomo85@gmail.com>
2019-07-14 18:58:09 +0200

    example plural #17

---
+++ examples/relay-hook-example/todo/js/components/TodoListPlural.js
@@ -0,0 +1,65 @@
+// @flow
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import MarkAllTodosMutation, {mutation} from '../mutations/MarkAllTodosMutation';
+import Todo from './Todo';
+
+import React from 'react';
+import {createFragmentContainer, graphql, type RelayProp} from 'react-relay';
+import QueryApp from '../query/QueryApp';
+import type {TodoList_user} from 'relay/TodoList_user.graphql';
+import { useFragment, useMutation } from 'relay-hooks';
+type Todos = $NonMaybeType<$ElementType<TodoList_user, 'todos'>>;
+type Edges = $NonMaybeType<$ElementType<Todos, 'edges'>>;
+type Edge = $NonMaybeType<$ElementType<Edges, number>>;
+type Node = $NonMaybeType<$ElementType<Edge, 'node'>>;
+
+type Props = {|
+  +relay: RelayProp,
+  +user: TodoList_user,
+|};
+
+const fragmentSpec = graphql`
+    fragment TodoListPlural_edges on TodoEdge @relay(plural: true) {
+          node {
+            complete
+            id
+            text
+          }
+        }
+  `;
+
+const TodoListPlural = (props) => {
+  const { refetch } = props;
+  const edges = useFragment(fragmentSpec, props.edges);
+
+  const nodes: $ReadOnlyArray<Node> =
+  edges 
+      ? Object.values(edges).map((edge: Edge) => edge.node)
+      : [];
+
+  return (
+    <section className="main">
+
+      <label htmlFor="toggle-all">Mark all as complete</label>
+
+      <ul className="todo-list">
+        {nodes.map((node: Node) => (
+          <Todo key={node.id} todo={node} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default TodoListPlural;

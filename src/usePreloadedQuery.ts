1419e2a968cd6773704544cd74924709411d5821
morrys <lorenzo.digiacomo85@gmail.com>
2020-05-12 20:18:09 +0200

    added loadQuery, loadLazyQuery & usePreloadedQuery

---
+++ src/usePreloadedQuery.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { OperationType } from 'relay-runtime';
+import { LoadQuery } from './loadQuery';
+import { RenderProps } from './RelayHooksType';
+import { useRelayEnvironment } from './useRelayEnvironment';
+
+export const usePreloadedQuery = <TOperationType extends OperationType = OperationType>(
+    loadQuery: LoadQuery,
+): RenderProps<TOperationType> => {
+    const [, forceUpdate] = useState();
+    const environment = useRelayEnvironment();
+
+    useEffect(() => {
+        const dispose = loadQuery.subscribe(forceUpdate);
+        return (): void => dispose();
+    }, [loadQuery]);
+
+    return loadQuery.getValue(environment) as RenderProps<TOperationType>;
+};

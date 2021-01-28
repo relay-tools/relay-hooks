a1200acd6e4412b811cbbc9f3bd278f6698baff0
Laurin Quast <laurinquast@googlemail.com>
2021-01-28 12:41:29 +0100

    feat: add a skip option for useSubscription
    Sometimes it is useful to skip a subscription as the variables for setting up the subscription might not be available yet.
    
    Closes https://github.com/relay-tools/relay-hooks/issues/131

--- src/useSubscription.ts
+++ src/useSubscription.ts
@@ -2,13 +2,22 @@ import { useEffect } from 'react';
 import { GraphQLSubscriptionConfig, requestSubscription, OperationType } from 'relay-runtime';
 import { useRelayEnvironment } from './useRelayEnvironment';
 
+type SubscriptionConfig = {
+  skip?: boolean;
+}
+
 export function useSubscription<TSubscriptionPayload extends OperationType = OperationType>(
     config: GraphQLSubscriptionConfig<TSubscriptionPayload>,
+    opts?: SubscriptionConfig
 ): void {
     const environment = useRelayEnvironment();
+    const skip = opts?.skip ?? false
 
     useEffect(() => {
+        if (skip) {
+          return
+        }
         const { dispose } = requestSubscription(environment, config);
         return dispose;
-    }, [environment, config]);
+    }, [environment, config, skip]);
 }

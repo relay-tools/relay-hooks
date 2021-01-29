f32214770f7d06ac8861cc34f2f792f7e09d5f43
n1ru4l <laurinquast@googlemail.com>
2021-01-29 14:07:20 +0100

    refactor: change typings according to suggestion

--- src/useSubscription.ts
+++ src/useSubscription.ts
@@ -2,10 +2,28 @@ import { useEffect } from 'react';
 import { GraphQLSubscriptionConfig, requestSubscription, OperationType } from 'relay-runtime';
 import { useRelayEnvironment } from './useRelayEnvironment';
 
-type SubscriptionConfig = {
+export type SubscriptionConfig = {
     skip?: boolean;
 };
 
+export type SkipSubscriptionConfig = {
+    skip: true;
+};
+
+export interface SkipGraphQLSubscriptionConfig<TSubscription extends OperationType>
+    extends Omit<GraphQLSubscriptionConfig<TSubscription>, 'variables' | 'subscription'> {
+    subscription?: GraphQLSubscriptionConfig<TSubscription>['subscription'];
+    variables?: TSubscription['variables'];
+}
+
+export function useSubscription<TSubscriptionPayload extends OperationType = OperationType>(
+    config: GraphQLSubscriptionConfig<TSubscriptionPayload>,
+    opts?: SubscriptionConfig,
+): void;
+export function useSubscription<TSubscriptionPayload extends OperationType = OperationType>(
+    config: SkipGraphQLSubscriptionConfig<TSubscriptionPayload>,
+    opts: SkipSubscriptionConfig,
+): void;
 export function useSubscription<TSubscriptionPayload extends OperationType = OperationType>(
     config: GraphQLSubscriptionConfig<TSubscriptionPayload>,
     opts?: SubscriptionConfig,

d8b347ecc8875c000b62f9b472b9f442b1241b3c
morrys <lorenzo.digiacomo85@gmail.com>
2022-01-29 17:39:16 +0100

    fix merge eslint

--- src/QueryFetcher.ts
+++ src/QueryFetcher.ts
@@ -122,7 +122,7 @@ export class QueryFetcher<TOperationType extends OperationType = OperationType> 
             this.fetcher.dispose();
             return;
         }
-        // eslint-disable-next-line @typescript-eslint/camelcase
+
         const { onComplete, onResponse } = options;
         let fetchHasReturned = false;
         const onNext = (_o: OperationDescriptor, snapshot: Snapshot): void => {

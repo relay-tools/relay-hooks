5d64c3c49c2eccf6f6272982ac0c3a072d6e5e7d
morrys <lorenzo.digiacomo85@gmail.com>
2025-07-15 16:14:14 +0200

    version 9.4.0

--- examples/relay-hook-example/pagination-nextjs-ssr/src/relay/createRelayEnvironment.ts
+++ examples/relay-hook-example/pagination-nextjs-ssr/src/relay/createRelayEnvironment.ts
@@ -15,7 +15,7 @@ function fetchQuery(operation, variables, _cacheConfig, _uploadables): any {
     const controller = new AbortController();
     let doAbort = true;
     const observer = Observable.create((sink) => {
-        sleep(2000).then(() =>
+        sleep(2).then(() =>
             fetch(endpoint, {
                 signal: controller.signal,
                 method: 'POST',
